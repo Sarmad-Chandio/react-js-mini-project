60e4f0e98eeed1c6c93b2b16b72c4127c26b1129
Sarmad Chandio <87301967+Sarmad-Chandio@users.noreply.github.com>
2026-02-01 15:29:31 +0500

    Add files via upload
    Components

---
+++ ExpenseFilter.tsx
@@ -0,0 +1,22 @@
+import Categories from "../../Categories";
+
+interface Props {
+  onSelectCategory: (Category: string) => void;
+}
+const ExpenseFilter = ({ onSelectCategory }: Props) => {
+  return (
+    <select
+      className="form-select"
+      onChange={(event) => onSelectCategory(event.target.value)}
+    >
+      <option value="">All Categories</option>
+      {Categories.map((category) => (
+        <option key={category} value={category}>
+          {category}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default ExpenseFilter;
